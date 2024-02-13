bee570e6ec084871e7e3b0d16753f79ac4d8be4e
jbsankarlal <jbsankarlal@gmail.com>
2024-02-13 19:23:53 +0530

    [Update]: added the useEffect and made the functionalities oworking properly

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,18 @@ function App() {
   const [time, setTime] = useState(0);
   const [running, setRunning] = useState(false);
 
+
   useEffect(() => {
     let interval;
     if (running) {
       interval = setInterval(() => {
         setTime((prevTime) => prevTime + 10)
-      }, 10)
+      }, 10);
+
     } else if (!running) {
-      clearInterval(interval);
+      clearInterval(interval)
     }
-    return () => clearInterval(interval);
+    return () => clearInterval(interval)
   }, [running])
 
   return (
